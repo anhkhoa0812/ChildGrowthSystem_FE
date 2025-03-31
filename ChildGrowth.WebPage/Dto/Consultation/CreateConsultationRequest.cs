f165b955655d1ff18e585b64877500f4dbe4a41c
anhkhoa0812 <tahoanganhkhoa2014@gmail.com>
2025-03-31 19:10:20 +0700

    feat: Add Consultations Page for Member

---
+++ ChildGrowth.WebPage/Dto/Consultation/CreateConsultationRequest.cs
@@ -0,0 +1,7 @@
+namespace ChildGrowth.WebPage.Dto.Consultation;
+
+public class CreateConsultationRequest
+{
+    public string Description { get; set; }
+    public string ConsultationType { get; set; }
+}
